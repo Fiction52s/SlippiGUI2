ce348ab86a381652716d2e539f660a4ea48ec385
Fiction52s <fiction52s@gmail.com>
2025-06-18 18:05:42 -0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SlippiGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.totalClipsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(321, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 152);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Create Save states";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(176, 355);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(115, 309);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Clip Length";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(21, 253);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 36);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Choose Export Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(20, 358);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Choose HMN player";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(598, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(845, 479);
+            this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
+            // 
+            // totalClipsLabel
+            // 
+            this.totalClipsLabel.AutoSize = true;
+            this.totalClipsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalClipsLabel.Location = new System.Drawing.Point(592, 3);
+            this.totalClipsLabel.Name = "totalClipsLabel";
+            this.totalClipsLabel.Size = new System.Drawing.Size(193, 31);
+            this.totalClipsLabel.TabIndex = 18;
+            this.totalClipsLabel.Text = "totalClipsLabel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(539, 180);
+            this.label1.TabIndex = 19;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 24);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1455, 531);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalClipsLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Melee Save State Clipper 0.1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label totalClipsLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,517 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+public enum Stage : UInt32
+{
+    FountainOfDreams = 002,
+    PokemonStadium = 003,
+    PrincessPeachsCastle = 004,
+    KongoJungle = 005,
+    Brinstar = 006,
+    Corneria = 007,
+    YoshisStory = 008,
+    Onett = 009,
+    MuteCity = 010,
+    RainbowCruise = 011,
+    JungleJapes = 012,
+    GreatBay = 013,
+    HyruleTemple = 014,
+    BrinstarDepths = 015,
+    YoshisIsland = 016,
+    GreenGreens = 017,
+    Fourside = 018,
+    MushroomKingdomI = 019,
+    MushroomKingdomII = 020,
+    Venom = 022,
+    PokeFloats = 023,
+    BigBlue = 024,
+    IcicleMountain = 025,
+    FlatZone = 027,
+    DreamLandN64 = 028,
+    YoshisIslandN64 = 029,
+    KongoJungleN64 = 030,
+    Battlefield = 031,
+    FinalDestination = 032,
+}
+
+public enum Character : byte
+{
+    Mario = 00,
+    Fox = 01,
+    CaptainFalcon = 02,
+    DonkeyKong = 03,
+    Kirby = 04,
+    Bowser = 05,
+    Link = 06,
+    Sheik = 07,
+    Ness = 08,
+    Peach = 09,
+    Popo = 10,
+    Nana = 11,
+    Pikachu = 12,
+    Samus = 13,
+    Yoshi = 14,
+    Jigglypuff = 15,
+    Mewtwo = 16,
+    Luigi = 17,
+    Marth = 18,
+    Zelda = 19,
+    YoungLink = 20,
+    DrMario = 21,
+    Falco = 22,
+    Pichu = 23,
+    MrGameAndWatch = 24,
+    Ganondorf = 25,
+    Roy = 26,
+}
+
+
+public enum CharacterColour : UInt32
+{
+    Red = 0,
+    Blue = 1,
+    // Add others...
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public struct OptionalCharacterColour
+{
+    public Character character;
+    public byte color;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct Time
+{
+    public UInt32 one;
+    public UInt32 two;
+    //public int hours;
+    //public int minutes;
+    //public int seconds;
+    // Update to match actual Rust Time struct
+}
+
+[StructLayout(LayoutKind.Sequential, Size = 31)]
+public struct Name
+{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 31)]
+    public byte[] bytes;
+}
+
+[StructLayout(LayoutKind.Sequential, Size = 10)]
+public struct ConnectCode
+{
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+    public byte[] bytes;
+}
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+struct GameInfo
+{
+    public Stage stage;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+    public byte [] ports_used;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+    public OptionalCharacterColour[] starting_character_colours;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+    public Name [] names;
+
+    [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+    public ConnectCode[] connect_codes;
+
+    public Time start_time;
+
+    public uint timer;
+    public int duration;
+
+    public byte version_major;
+    public byte version_minor;
+    public byte version_patch;
+}
+
+namespace SlippiGUI
+{
+    public partial class Form1 : Form
+    {
+        private string[] slpPaths;
+        private string exportFolderPath;
+        private BindingList<SlippiEntry> entries;
+        ToolTip exportPathToolTip;
+        int numTotalClips;
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.Add("Player 1");
+            comboBox1.Items.Add("Player 2");
+            //comboBox1.SelectedIndex = 0;
+            //comboBox1.SelectedIndexChanged -= comboBox1_SelectedIndexChanged;
+
+            comboBox1.SelectedIndex = 0;  // Set default without triggering event
+
+            label3.Text = "no export path set";
+
+            textBox1.Text = 360.ToString();
+
+            entries = new BindingList<SlippiEntry>();
+
+            dataGridView1.AutoGenerateColumns = false;
+
+            dataGridView1.Columns.Clear();
+
+            exportPathToolTip = new ToolTip();
+            exportPathToolTip.SetToolTip(label3, "no export path set");
+            //    public string path { get; set; }
+            //public string gameName { get; set; }
+            //public Character p1 { get; set; }
+            //public Character p2 { get; set; }
+            //public Stage stage { get; set; }
+            //public string dateAndTime { get; set}
+            //public string matchLength { get; set}
+
+            // Define column for 'name'
+
+            AddGameInfoColumn("File Path", "filePath", 200, true);
+            AddGameInfoColumn("Game Name", "gameName", 200, false);
+            AddGameInfoColumn("Player 1", "p1", 100, true);
+            AddGameInfoColumn("Player 2", "p2", 100, true);
+            AddGameInfoColumn("Stage", "stage", 100, true);
+            AddGameInfoColumn("Number of Save States", "numClips", 100, true);
+
+            totalClipsLabel.Text = "Total save states: " + 0;
+            numTotalClips = 0;
+
+            label3.AutoSize = false;
+            label3.AutoEllipsis = true;
+            label3.Width = 200;
+
+            if (File.Exists("config.txt"))
+            {
+                exportFolderPath = File.ReadAllText("config.txt");
+                label3.Text = exportFolderPath;
+                exportPathToolTip.SetToolTip(label3, exportFolderPath);
+            }
+
+            //AddGameInfoColumn("Date and Time", "dateAndTime", 100, true);
+            //AddGameInfoColumn("Match Length", "matchLength", 100, true);
+
+            // public UInt32 clipFrame { get; set; }
+            //public UInt32 clipLength { get; set; }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //CheckForAllClips();
+
+            int clipLength = 360;
+            int foundLength = 0;
+
+            if (int.TryParse(textBox1.Text.Trim(), out foundLength))
+            {
+                clipLength = foundLength;
+                // It's a valid integer — use it
+                //Console.WriteLine($"User entered: {parsedValue}");
+            }
+            else
+            {
+                // Invalid input — optionally do nothing or handle it
+                // Console.WriteLine("Invalid number, ignoring input.");
+                textBox1.Text = clipLength.ToString();
+            }
+
+            if( exportFolderPath == null )
+            {
+                MessageBox.Show("export path not set");
+                return;
+            }
+
+            foreach( var item in entries )
+            {
+                CreateSavestates(exportFolderPath, item.filePath, item.gameName, clipLength);
+            }
+
+            MessageBox.Show("Created " + numTotalClips + " save states in " + exportFolderPath);
+            // Remove invalid file name characters
+
+            //later make sure the gameName is safe before validating
+
+            // Or use Regex:
+            //string safeName = string.Join("_", textBox2.Text.Split(Path.GetInvalidFileNameChars()));
+            //safeName = Regex.Replace(safeName, @"[<>:""/\\|?*]", "_");
+
+            //// Optional: Fallback if name is empty after cleaning
+            //if (string.IsNullOrWhiteSpace(safeName))
+            //{
+            //    MessageBox.Show("game name is not valid");
+            //    return;
+            //}
+    }
+
+        private void CreateSavestates(string exportFolder, string currSlp, string gameName, int clipLength)
+        {
+            byte[] exportFolder_utf8 = Encoding.UTF8.GetBytes(exportFolder + "\0");
+            IntPtr exportFolderPtr = Marshal.AllocHGlobal(exportFolder_utf8.Length);
+            Marshal.Copy(exportFolder_utf8, 0, exportFolderPtr, exportFolder_utf8.Length);
+
+            byte[] slpPath_utf8 = Encoding.UTF8.GetBytes(currSlp + "\0");
+            IntPtr slpPathPtr = Marshal.AllocHGlobal(slpPath_utf8.Length);
+            Marshal.Copy(slpPath_utf8, 0, slpPathPtr, slpPath_utf8.Length);
+
+            byte[] gameName_utf8 = Encoding.UTF8.GetBytes(gameName + "\0");
+            IntPtr gameNamePtr = Marshal.AllocHGlobal(gameName_utf8.Length);
+            Marshal.Copy(gameName_utf8, 0, gameNamePtr, gameName_utf8.Length);
+
+            Program.create_savestates(exportFolderPtr, slpPathPtr, gameNamePtr, comboBox1.SelectedIndex, clipLength);
+
+            //MessageBox.Show("Created " + listBox1.Items.Count + " save states in " + exportFolder + " with game name " + gameName + " with clip length " + clipLength);
+        }
+
+        private void SetAllInfo()
+        {
+            if (entries == null)
+                return;
+
+            entries.Clear();
+            for( int i = 0; i < slpPaths.Length; ++i )
+            {
+                AddEntry(slpPaths[i]);
+            }
+
+            int totalClips = 0;
+            foreach (var item in entries)
+            {
+                totalClips += item.numClips;   
+            }
+
+            totalClipsLabel.Text = "Total save states: " + totalClips;
+            numTotalClips = totalClips;
+
+            dataGridView1.DataSource = entries;
+
+            //entries[0].name = "FF";
+           // entries[0].p1 = Character.Falco;
+            //entries.First().name = "F";
+            //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+           
+
+            //dataGridView1.Columns["name"].ReadOnly = false;
+           // dataGridView1.Columns["p1"].ReadOnly = true;
+           // dataGridView1.Columns["p2"].ReadOnly = true;
+        }
+
+        private void AddEntry(string path)
+        {
+            IntPtr namePtr = Marshal.StringToHGlobalAnsi(path);
+
+            try
+            {
+                IntPtr gameInfoPtr = Program.read_info(namePtr);
+
+                if (gameInfoPtr == IntPtr.Zero)
+                {
+                    Console.WriteLine("Failed to read info (null pointer returned)");
+                    return;
+                }
+
+                GameInfo test = Marshal.PtrToStructure<GameInfo>(gameInfoPtr);
+
+                SlippiEntry se = new SlippiEntry();
+                se.filePath = path;
+                se.gameName = Path.GetFileNameWithoutExtension(path);
+                se.p1 = test.starting_character_colours[0].character;
+                se.p2 = test.starting_character_colours[1].character;
+                se.stage = test.stage;
+                se.clipFrames = CheckForClips(path);
+                se.numClips = se.clipFrames.Length;
+                //CheckForClips
+                //se.matchLength = test.
+
+                entries.Add(se);
+                //string n = Encoding.UTF8.GetString(test.connect_codes[1].bytes, 0, 10).TrimEnd('\0');
+
+                //label1.Text = "Player 1: " + test.starting_character_colours[0].character.ToString()
+                //    + "\n" + "Player 2: " + test.starting_character_colours[1].character.ToString()
+                //    + "\n" + "Stage: " + test.stage.ToString()
+                //    + "\n" + "Max duration (seconds): " + test.duration;
+
+                //label1.Show();
+                //label2.Show();
+                //label3.Show();
+                //label4.Show();
+                //label5.Show();
+                //label6.Show();
+
+                //textBox1.Show();
+                //textBox2.Show();
+
+                ////button2.Show();
+
+                //comboBox1.Show();
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(namePtr);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SetAllInfo();
+            //CheckForAllClips();
+        }
+
+        private int[] CheckForClips( string path )
+        {
+            byte[] utf8 = Encoding.UTF8.GetBytes(path + "\0");
+            IntPtr namePtr = Marshal.AllocHGlobal(utf8.Length);
+            Marshal.Copy(utf8, 0, namePtr, utf8.Length);
+
+            UIntPtr len;
+            IntPtr arrayPtr = Program.check_clips(namePtr, out len, comboBox1.SelectedIndex);
+            int[] result = new int[(int)len];
+
+            if (arrayPtr != IntPtr.Zero)
+            {
+                Marshal.Copy(arrayPtr, result, 0, (int)len);
+                Program.free_clip_array(arrayPtr, len);
+            }
+
+            Marshal.FreeHGlobal(namePtr);
+
+            return result;
+
+            //listBox1.Items.Clear();
+            //foreach (int num in result)
+            //{
+            //    listBox1.Items.Add(num);
+            //}
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                IsFolderPicker = true,
+                Title = "Select a folder"
+            };
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string selectedPath = dialog.FileName;
+                exportFolderPath = selectedPath;
+                label3.Text = selectedPath;
+                exportPathToolTip.SetToolTip(label3, exportFolderPath);
+                File.WriteAllText("config.txt", exportFolderPath);
+                //textBoxFolder.Text = selectedPath;
+            }
+            else
+            {
+
+            }
+
+            //folderBrowserDialog1.Description = "Select a folder to export to";
+            ////folderBrowserDialog1.UseDescriptionForTitle = true; // Optional, for newer Windows
+
+            //if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    string selectedPath = folderBrowserDialog1.SelectedPath;
+            //    //MessageBox.Show("Export folder: " + selectedPath);
+            //    textBox2.Text = selectedPath;
+
+            //    exportFolderPath = selectedPath;
+
+            //    File.WriteAllText("config.txt", exportFolderPath);
+
+            //    // Use selectedPath for your export logic
+            //}
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Length > 0)
+            {
+                if( slpPaths == null )
+                {
+                    slpPaths = files;
+                }
+                else
+                {
+                    slpPaths = slpPaths.Union(files).ToArray();
+                }
+                
+
+                SetAllInfo();
+
+               // CheckForAllClips();
+
+                //button1.Show();
+
+                //listBox1.Show();
+            }
+        }
+
+        private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void AddGameInfoColumn(string headerStr, string propertyName, int width, bool readOnly)
+        {
+            var col = new DataGridViewTextBoxColumn();
+            col.HeaderText = headerStr;
+            col.DataPropertyName = propertyName; // Must match property in your class
+            col.Width = width;
+            col.ReadOnly = readOnly;
+            if( propertyName == "gameName")
+            {
+                col.MaxInputLength = 20;
+            }
+            dataGridView1.Columns.Add(col);
+        }
+    }
+}
+
+public class SlippiEntry
+{
+    public string filePath { get; set; }
+    public string gameName { get; set; }
+    public Character p1 { get; set; }
+    public Character p2 { get; set; }
+    public Stage stage { get; set; }
+    public int numClips { get; set; }
+    public string dateAndTime { get; set; }
+    public string matchLength { get; set; }
+    public int[] clipFrames { get; set; }
+}
